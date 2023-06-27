9dea9139a4a5bc9e85bba516d6b1677ce3212acf
jesusvite <nicolasvitejesus@gmail.com>
2023-06-27 09:11:28 -0600

    puede realizar con exito la API

--- src/assets/main.js
+++ src/assets/main.js
@@ -17,23 +17,23 @@ async function getData(url){
 (
     async function (){
         try {
-            const response = await getData(linkVideos);
-            const result = `${video.meta.map(video => `
+            const videos = await getData(linkVideos);
+            const view = `${videos.data.map(video => `
             <div class="group relative">
                 <div
                     class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="${video.meta.thumbnail.url}" alt="" class="w-full">
+                    <img src="${video.thumbnail[2].url}" alt="${video.description}" class="w-full">
                 </div>
                 <div class="mt-4 flex justify-between">
                     <h3 class="text-sm text-gray-700">
                     <span aria-hidden="true" class="absolute inset-0"></span>
-                    ${video.meta.title}
+                    ${video.title}
                     </h3>
                 </div>
             </div>
-            `).slice(0,4).join('')}
+            `).slice(0,16).join('')}
             `;
-            content.innerHTML = result;
+            content.innerHTML = view;
           
         } catch (error) {
             console.error(error);
